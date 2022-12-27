713406b2f2ab579ce75257fd0a02fe9bf3c381c3
halolutfii <halolutfii@gmail.com>
2022-12-27 18:01:58 +0700

    Tugas Pengenalan UUID

---
+++ crowdfunding-website/app/Models/Roles.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traints\UsesUuid;
+
+class Roles extends Model
+{
+    use HasFactory, UsesUuid;
+    protected $fillable=['name'];
+
+    public function users(){
+        return $this->hasMany(User::class, 'role_id');
+    }
+}
+
